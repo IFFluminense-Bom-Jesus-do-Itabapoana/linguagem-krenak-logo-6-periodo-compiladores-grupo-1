343b4cd0165656e5ef184b06dd63813019091eb2
Maycon J. Deláqua <107778965+MayconJD@users.noreply.github.com>
2024-05-06 22:40:41 -0300

    Test Lexer Nome da Função

--- krenaklogo/src/test/java/br/edu/iff/bji/ec/compiladores/krenaklogo/KrenakLogoLexerTest.java
+++ krenaklogo/src/test/java/br/edu/iff/bji/ec/compiladores/krenaklogo/KrenakLogoLexerTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class KrenakLogoLexerTest {
     
     @Test
-    public void testNumberToken() {
+    public void testNumeroToken() {
         CharStream cs = CharStreams.fromString("123");
         KrenakLogoLexer lexer = new KrenakLogoLexer(cs);
         Token token = lexer.nextToken();
