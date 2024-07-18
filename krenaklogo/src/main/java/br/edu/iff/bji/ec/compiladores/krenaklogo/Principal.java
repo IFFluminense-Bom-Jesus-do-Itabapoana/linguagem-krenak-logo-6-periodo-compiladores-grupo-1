f7fbe6a0b7d926bb268daaa7760fa0ba1129d8bf
luan-lulucricri <luanpcrisostomo@gmail.com>
2024-07-18 11:58:57 -0300

    fix git ignore

---
+++ krenaklogo/src/main/java/br/edu/iff/bji/ec/compiladores/krenaklogo/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.iff.bji.ec.compiladores.krenaklogo;
+
+/**
+ *
+ * @author fbarr
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
